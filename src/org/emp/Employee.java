39fa4fc8d7b92063315395f2456cea54f7e5e6e4
Vigneshpsm8248 <vigneshvicky.psm@gmail.com>
2023-01-11 12:18:31 +0530

    Cloning

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,20 @@
+package org.emp;
+
+public class Employee {
+ 
+	private void empId() {
+		System.out.println("Emp ID:12345");
+	}
+	private void empName() {
+		System.out.println("Emp Name:Vignesh");
+	}
+	
+	
+	public static void main(String args[]) {
+		Employee empInfo = new Employee();
+		empInfo.empId();
+		
+		
+	
+	}
+}
