6835931af48d597bd8329623b5347c0b5f2f72a0
Vigneshpsm8248 <vigneshvicky.psm@gmail.com>
2023-01-12 11:44:00 +0530

    Updated Vicky

--- src/org/emp/Employee.java
+++ src/org/emp/Employee.java
@@ -9,18 +9,21 @@ public class Employee {
 		System.out.println("Emp Name:Vignesh");
 	}
 	private void method2() {
-		// TODO Auto-generated method stub
+	System.out.println("Vignesh Vicky");
 
 	}
 	private void method() {
 		// TODO Auto-generated method stub
 
 	}
-	
+	private void utut() {
+		// TODO Auto-generated method stub
+
+	}
 	public static void main(String args[]) {
 		Employee empInfo = new Employee();
 		empInfo.empId();
-		
+		empInfo.method2();
 	}
 	
 	
